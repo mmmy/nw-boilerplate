c3d515dcd5c1b8bd2439b0723efcaa73ee0634bb
Xiaofan Wu <xfanwu@gmail.com>
2016-04-06 17:17:35 +0800

    implement Devtools and envify

---
+++ src/containers/Root.dev.js
@@ -0,0 +1,38 @@
+import React, {PropTypes} from 'react';
+import MainContainer from './MainContainer';
+//import { connect } from 'react-redux';
+import { toggleStockView } from '../flux/actions/layoutActions';
+import Header from './Header';
+import RightToolBar from './RightToolBar';
+import CoreApp from './CoreApp';
+import DevTools from './DevTools';
+
+import StockView from './StockView';
+import SearchReport from './SearchReport';
+
+class Root extends React.Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render(){
+
+		const { stockView } = this.props;
+
+		return <MainContainer>
+
+			<Header />
+
+			<RightToolBar />
+
+			<CoreApp />
+
+      <DevTools />
+
+		</MainContainer>;
+	}
+
+}
+
+export default Root;
