75489f0e36e41f5cdb2d409762c31207679715e9
Xiaofan Wu <xfanwu@gmail.com>
2016-04-08 16:52:57 +0800

    Split chart layout into 2 vertical

--- src/main.js
+++ src/main.js
@@ -12,28 +12,39 @@ import GenerateStore from './flux/GenerateStore';
 let store = GenerateStore();
 window.store = store;
 
+let setChartLayout = () => {
+  setTimeout(() => {
+    var tv = document[window.document.getElementsByTagName('iframe')[0].id];
+    if (tv.Q5)
+      tv.W76.setChartLayout(tv.Q5, '2v');
+    else
+      getTV();
+  }, 3.5E3)
+}
 
 Promise.all([
-	new Promise((resolve) => {
-		if(window.addEventListener) {
-			window.addEventListener('DOMContentLoaded', resolve);
-		} else {
-			window.attachEvent('onLoad', resolve);
-		}
-	})
-]).then(() => {
-	// window.nw && window.nw.Window.get()
-	// var win = ;
-	// win.showDevTools();
-
-	if (process.env.NODE_ENV === 'development') {
-      var head = document.getElementsByTagName('head')[0];
-      var script = document.createElement('script');
-      script.type = 'text/javascript';
-      script.src = 'http://localhost:35729/livereload.js';
-      head.appendChild(script);
+  new Promise((resolve) => {
+    if(window.addEventListener) {
+      window.addEventListener('DOMContentLoaded', resolve);
+    } else {
+      window.attachEvent('onLoad', resolve);
     }
-	require('nw.gui').Window.get().showDevTools();
-	init();
-	ReactDOM.render(<Provider store={store}><Root /></Provider>, document.getElementById('app'));
+  })
+]).then(() => {
+  init();
+
+  if (process.env.NODE_ENV === 'development') {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'http://localhost:35729/livereload.js';
+    head.appendChild(script);
+  }
+  ReactDOM.render(<Provider store={store}><Root /></Provider>, document.getElementById('app'));
+
+  if (process.env.NODE_ENV === 'development') require('nw.gui').Window.get().showDevTools();
+
+})
+.then(() => {
+  setChartLayout();
 });
