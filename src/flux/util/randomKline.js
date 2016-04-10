50cb0e677f7ce234c2502bc290d25e86bb5a325d
yangqi@ubuntu <531905408@qq.com>
2016-04-10 14:34:50 +0800

    add test data

---
+++ src/flux/util/randomKline.js
@@ -0,0 +1,26 @@
+var data = require('../../../samples/ss600000-s.json');
+
+let len = data.length,
+	maxIndex = len - 200;
+
+var randomKline = function(){
+	let start = Math.round(Math.random() * maxIndex);
+	let dataLen = 5 + Math.round(Math.random() * 360);
+	return data.slice(start, start + dataLen);
+};
+
+var randomPartterns = function(n) {
+	var partterns = [];
+	for (let i=0; i<n; i++) {
+		partterns.push({
+			symbol: '000001',
+			similarity: Math.random(),
+			kLine: randomKline(),
+		});
+	}
+	return partterns;
+};
+
+module.exports = {
+	randomPartterns,
+}
