4cbe8fb5a9f5e5e12c96094e59b3290daded1f4a
Xiaofan Wu <xfanwu@gmail.com>
2016-04-14 18:17:08 +0800

    clean code in layoutAction

--- src/flux/actions/layoutActions.js
+++ src/flux/actions/layoutActions.js
@@ -1,31 +1,21 @@
 import * as types from '../constants/ActionTypes';
- 
 
-let toggleStockView = function toggleStockView() {
-	// return (dispatch) => {
-	// 	dispatch(() => {
-	// 		return {
-	// 			type: types.TOGGLE_STOCK_VIEW
-	// 		}	
-	// 	});
-	// }
-	return (dispatch) => {
-		dispatch({
-			type: types.TOGGLE_STOCK_VIEW,
-		})
-	}
-	// return {
-	// 		type: types.TOGGLE_STOCK_VIEW,
-	// 		}
+
+var toggleStockView = function toggleStockView() {
+  return (dispatch) => {
+    dispatch({
+      type: types.TOGGLE_STOCK_VIEW,
+    })
+  }
 };
 
-let waitingForPatterns = function() {
-	return {
-		type: types.WAITING_PATTERNS
-	};
+var waitingForPatterns = function() {
+  return {
+    type: types.WAITING_PATTERNS
+  };
 };
 
 module.exports = {
-	toggleStockView,
-	waitingForPatterns,
+  toggleStockView,
+  waitingForPatterns,
 }
