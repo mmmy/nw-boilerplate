8ab0e47eedb0d470dc9822ec02d52e0e6aeb5b33
Xiaofan Wu <xfanwu@gmail.com>
2016-04-14 14:13:39 +0800

    ComparatorPredition component

---
+++ src/components/ComparatorPredition.js
@@ -0,0 +1,43 @@
+import React, { PropTypes } from 'react';
+import classNames from 'classnames';
+
+const propTypes = {
+
+};
+
+const defaultProps = {
+
+};
+
+class ComparatorPredition extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	componentDidMount() {
+
+	}
+
+	componentWillReceiveProps(){
+
+	}
+
+	shouldComponentUpdate(){
+		return true;
+	}
+
+	componentWillUnmount(){
+
+	}
+
+	render(){
+		return (<div className=""></div>);
+	}
+}
+
+ComparatorPredition.propTypes = propTypes;
+ComparatorPredition.defaultProps = defaultProps;
+
+export default ComparatorPredition;
