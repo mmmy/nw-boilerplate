4eb0b0ace793dba83f9620d6aa4d23033324fbaa
Xiaofan Wu <xfanwu@gmail.com>
2016-06-02 20:50:26 +0800

    Add echart margin

--- src/components/ComparatorPrediction.js
+++ src/components/ComparatorPrediction.js
@@ -131,7 +131,7 @@ class ComparatorPrediction extends React.Component {
     window.eChartMaxValue = maxValue;
     window.eChartMinValue = minValue;
     let scaleMax = Math.max(Math.abs(maxValue), Math.abs(minValue));
-    window.eChartScale = scaleMax * 1.3;
+    window.eChartScale = scaleMax * 1.2;
 
     return eChartSeriesData;
   }
