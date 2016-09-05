50d74be611cc5908a7708711b0e67fe70bd5fd77
Liang Zexian <wythend@LiangdeMacBook-Air.local>
2016-09-05 18:55:38 +0800

    futures

--- src/backend/config.js
+++ src/backend/config.js
@@ -6,7 +6,7 @@ export default {
 		host: '120.24.71.4',  //'192.168.0.20',
 		port: 17654,
 		path: '/match',
-		url: 'http://120.24.71.4:17654/match',
+		url: 'http://112.74.17.46:17654/match',//'http://120.24.71.4:17654/match',
 		method: 'POST',
 		agent: false
 	},
@@ -15,7 +15,7 @@ export default {
 		host: /*'120.24.71.4',//*/'112.74.17.175',
 		port: 15501,
 		path: '/query',
-		url: 'http://120.24.71.4:15501/query',
+		url: 'http://112.74.17.46:25501/query', //'http://120.24.71.4:15501/query',
 		method: 'POST',
 	},
 
@@ -24,6 +24,13 @@ export default {
 		port: 3000,
 		path: '/file_chunk',
 		method: 'GET',
-	}
+	},
 
+    symbolListOptions: {
+        host: "http://112.74.17.46",
+        port: 25501,
+        path: "querysymbols",
+        url: "http://112.74.17.46:25501/querysymbols",
+        method: "POST"
+    }
 }
