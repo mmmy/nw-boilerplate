d82a3a985d18f540b3953c3dc8608dc884d49f51
Liang Zexian <wythend@LiangdeMacBook-Air.local>
2016-09-14 07:19:51 +0800

    增加通用的Cache，获取symbolList先获取groupCode,再逐一获取各种。

--- src/backend/udf.js
+++ src/backend/udf.js
@@ -1,5 +1,6 @@
 import request from './request';
 import config from './config';
+var Cache  = require("./Cache");
 
 let getSymbolHistory = (postData, callback, errorCallback) => {
   const { patternOptions } = config;
@@ -16,23 +17,100 @@ let getSymbolHistory = (postData, callback, errorCallback) => {
   request(options, requestCb, errorCb, JSON.stringify(postData));
 };
 
-let getGroupCode = (callback, errorCallback) => {
+let getGroupCode = (callback) => {
   const { groupOptions } = config;
   const options = { ...groupOptions };
-
+  
   const requestCb = (result) => {
+    result = dealGroupCode(result);
+    Cache.setToFile(result, 'groupCode');
     callback && callback(result);
   };
 
   const errorCb = (err) => {
-    callback && callback(err);
+    callback && callback(Cache.getFromFile('groupCode'));
+  };
+
+  if (Cache.isLegal('groupCode')) {
+    callback && callback(Cache.getFromFile('groupCode'));
+  } else {
+    request(options, requestCb, errorCb, '');
+  }
+};
+
+let dealGroupCode = (data) => {
+  var groups = [];
+  JSON.parse(data)['groups'].forEach(
+    function (group) {
+      groups.push(group['code']);
+    });
+  return JSON.stringify(groups);
+};
+
+let getAllSymbolsList = (callback) => {
+  let cb = (_groupCodes) => {
+    console.log(_groupCodes);
+    var promises = [];
+    JSON.parse(_groupCodes).forEach(function(code) {
+      
+      promises.push(new Promise((resolve, reject) => {
+        getOneSymbolList({fileName: code, postData: { 'groupCode': code } }, function (data) {
+          resolve(data);
+        });
+      }));
+
+    });
+
+    Promise.all(promises).then(function(result) {
+      var arr = [];
+      console.log('promise result', result);
+      result.forEach( function(r) {
+        console.log(typeof(r));
+        console.log(typeof(JSON.parse(r)));
+        console.log(JSON.parse(r)[0]);
+        arr = arr.concat(JSON.parse(r));
+      });
+      callback(JSON.stringify(arr));
+    });
+
+  };
+  getGroupCode(cb);
+};
+
+
+let getOneSymbolList = (data, callback) => {
+  console.log('get one');
+  var fileName = data.fileName;
+  var postData = data.postData;
+  const { symbolListOptions } = config;
+  const options = { ...symbolListOptions };
+
+  const requestCb = (result) => {
+    result = dealSymbolList(result);
+    Cache.setToFile(result, fileName);
+    callback && callback(result);
+  };
+
+  const errorCb = (err) => {
+    callback && callback(Cache.getFromFile(fileName));
   }
 
-  request(options, requestCb, errorCb, JSON.stringify([]));
+  if (Cache.isLegal(fileName)) {
+    callback && callback(Cache.getFromFile(fileName));
+  } else {
+    request(options, requestCb, errorCb, JSON.stringify(postData));
+  }
+};
+
+let dealSymbolList = (data) => {
+  return JSON.stringify(JSON.parse(data)['symbols']);
 };
 
 
 let getSymbolList = (postData, callback) => {
+  console.log("getSymbolList first");
+  getAllSymbolsList(callback);/*
+  return;
   const { symbolListOptions } = config;
   const options = { ...symbolListOptions };
 
@@ -52,7 +130,7 @@ let getSymbolList = (postData, callback) => {
     return cache;
   } else {
     request(options, requestCb, errorCb, JSON.stringify(postData));
-  }
+  }*/
 };
 
 let getSymbolSearchResult = (postData, callback) => {
@@ -83,7 +161,6 @@ let getLoginInfo = (postData, callback) => {
     callback && callback(err);
   }
 
-  console.log(options);
   request(options, requestCb, errorCb, postData);
 }
 
@@ -109,5 +186,6 @@ module.exports = {
   getSymbolSearchResult,
   getSymbolList,
   getLoginInfo,
-  getLogoutInfo
+  getLogoutInfo,
+  getAllSymbolsList,
 }
