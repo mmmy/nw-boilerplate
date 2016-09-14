9600fac56f09208ed3b088d9b30f4ed02ccd69e1
Liang Zexian <wythend@LiangdeMacBook-Air.local>
2016-09-14 11:41:44 +0800

    配置修改

--- src/backend/config.js
+++ src/backend/config.js
@@ -1,8 +1,7 @@
 export default {
 
-<<<<<<< HEAD
     searchOptions: {            //鎼滅储閰嶇疆
-        host: '139.196.226.133',  //'192.168.0.20', 
+        host: '139.196.226.133',  //'192.168.0.20',
         port: 30020,
         path: '/match',
         url: 'http://112.74.17.46:17654/match', //鏈熻揣
@@ -26,26 +25,6 @@ export default {
         path: '/file_chunk',
         method: 'GET',
     },
-=======
-	searchOptions: { 	    //搜索配置
-		host: '120.24.71.4',  //'192.168.0.20',
-		port: 17654,
-		path: '/match',
-		// url: 'http://112.74.17.46:17654/match', //期货
-		url: 'http://120.24.71.4:17654/match',  //btb
-		method: 'POST',
-		agent: false
-	},
-
-	patternOptions: {  	    //获取k线数据配置
-		host: /*'120.24.71.4',//*/'112.74.17.175',
-		port: 15501,
-		path: '/query',
-		// url: 'http://112.74.17.46:25501/query',  //期货
-		url: 'http://120.24.71.4:15501/query',  //btb
-		method: 'POST',
-	},
->>>>>>> 54456d55fad46fbd8ea3ed0ce55faec8df65619c
 
     groupOptions: {
     	host: '112.74.17.46',
