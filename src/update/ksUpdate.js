911357d879f8b44086a6f9dd4c8f68669ab47c63
Liang Zexian <wythend@LiangdeMacBook-Air.local>
2016-09-23 14:58:58 +0800

    add update file

---
+++ src/update/ksUpdate.js
@@ -0,0 +1,39 @@
+import updateConfig from './updateConfig';
+//var gui = require('nw.gui');
+//gui.Window.get().showDevTools();
+//document.getElementById('inf').innerHTML = 'good';
+
+//download new version of app in tmp
+//unpack
+//run updater
+//updater will copy itself to origin directory
+//updater will run app
+var updater = require('./auto-updater-win');
+var updateURL = updateConfig.URL;
+updater.setFeedURL(updateURL);
+try {
+	updater.checkForUpdates();
+	updater.on('error', function() {
+		console.log('error');
+	});
+	updater.on('update-not-available', function() {
+		console.log("update-not-available, it's latest")
+	});
+	updater.on('checking-for-update', function() {
+		console.log('checking-for-update')
+	});
+	updater.on('update-available', function() {
+		console.log('update-available')
+	});
+	updater.on('update-downloaded', function(a, b, c, d, e, f) {
+		//console.log('update-downloaded');
+		//console.log(a, b, c, d, e, f);
+		//f();
+		//console.log('ready to close the app now');
+		//gui.Window.get().close(true);
+	});
+} catch (err) {
+	console.log(err);
+} finally {
+
+}
---
+++ src/update/updateConfig.js
@@ -0,0 +1,3 @@
+export default {
+	'URL': 'http://www.baidu.com/releases'
+}
