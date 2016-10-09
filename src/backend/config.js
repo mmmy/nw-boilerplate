6d67febc2e27586921f74d4b2dbc16de31524676
yangqi <yangqi@yangqideMacBook-Air.local>
2016-10-09 15:16:44 +0800

    use domain

--- src/backend/config.js
+++ src/backend/config.js
@@ -1,10 +1,18 @@
+
+const DOMAIN = 'http://service.stone.io';
+
+let getURL = function(port, path) {
+    return `${DOMAIN}:${port}/${path}`;
+};
+
 export default {
 
     searchOptions: {            //搜索
         host: '139.196.226.133',  //'192.168.0.20',
         port: 30020,
         path: '/match',
-        url: 'http://139.196.226.133:30020/match', //stock, Future
+        url: getURL(30020, 'match'),
+        // url: 'http://139.196.226.133:30020/match', //stock, Future
         // url: 'http://112.74.17.46:17654/match', //stock, Future
         // url: 'http://120.24.71.4:17654/match',  //btb
         method: 'POST',
@@ -15,7 +23,8 @@ export default {
         host: /*'120.24.71.4',//*/'139.196.226.133',
         port: 15501,
         path: '/query',
-        url: 'http://139.196.226.133:30011/query',  //stock, Future
+        url: getURL(30011, 'query'),
+        // url: 'http://139.196.226.133:30011/query',  //stock, Future
         // url: 'http://112.74.17.46:25501/query',  //stock, Future
         // url: 'http://192.168.0.102:35501/query',  //stock, Future
         // url: 'http://120.24.71.4:15501/query',  //btb
@@ -33,7 +42,8 @@ export default {
     	host: '112.74.17.46',
         port: 25501,
         path: '/groups',
-        url: 'http://139.196.226.133:30011/groups',
+        url: getURL(30011, 'groups'),
+        // url: 'http://139.196.226.133:30011/groups',
         // url: 'http://112.74.17.46:25501/groups',
         method: 'GET'
     },
@@ -42,18 +52,21 @@ export default {
         host: "http://139.196.226.133",
         port: 25501,
         path: "querysymbols",
-        url: "http://139.196.226.133:30011/querysymbols",
+        url: getURL(30011, 'querysymbols'),
+        // url: "http://139.196.226.133:30011/querysymbols",
         // url: "http://112.74.17.46:25501/querysymbols",
         method: "POST"
     },
 
 	loginOptions: {
-		url: 'http://112.74.17.46:30030/api/login',
+        url: getURL(30030, 'api/login'),
+		// url: 'http://112.74.17.46:30030/api/login',
 		method: 'POST'
 	},
 
 	logoutOptions: {
-		url: 'http://112.74.17.46:30030/api/logout',
+        url: getURL(30030, 'api/logout'),
+		// url: 'http://112.74.17.46:30030/api/logout',
 		method: 'POST'
 	},
 }
