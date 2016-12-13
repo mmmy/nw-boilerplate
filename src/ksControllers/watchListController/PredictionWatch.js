4386cb029966444850c7445940ccd2565d57e0ca
yangqi <yangqi@yangqideMacBook-Air.local>
2016-12-13 21:45:30 +0800

    udpate styles

--- src/ksControllers/watchListController/PredictionWatch.js
+++ src/ksControllers/watchListController/PredictionWatch.js
@@ -100,7 +100,7 @@ PredictionWatch.prototype._init = function() {
 
 	this._$parent.find(".watchlist-prediction-wrapper.add").before(this._$root);
 	//k线图
-	this._predictionWidget = new PredictionWidget(this._$root.find('.prediction-chart')[0], {showRange: false});
+	this._predictionWidget = new PredictionWidget(this._$root.find('.prediction-chart')[0], {showRange: false, slient: true});
 	this._predictionWidget.onHoverKline((index, data)=>{
 
 	})
